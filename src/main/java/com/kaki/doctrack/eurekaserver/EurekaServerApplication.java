2c2c231dbff556694bb30557551b9d8b75cf0582
Joaquin Navarro <alfian1991@gmail.com>
2024-06-05 19:26:47 -0400

    First Commit

---
+++ src/main/java/com/kaki/doctrack/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,18 @@
+package com.kaki.doctrack.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Eureka Server Application
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
